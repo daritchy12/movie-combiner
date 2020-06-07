57df20d85796bb61b32fb5a5202dfc7bb6883f7e
Junior Jean <44121748+L33t-Rose@users.noreply.github.com>
2020-06-07 15:40:20 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-    console.log('Jquery Works Here');
-})
+let bookData = {
+    bookmarks:[]
+};
 window.onload = function(){
     if(localStorage.getItem('current')){
         console.log(localStorage.getItem('current'))
@@ -9,4 +9,40 @@ window.onload = function(){
     else{
         localStorage.setItem('current',null)
     }
+
+    if(!localStorage.getItem('bookmarks')){
+        localStorage.setItem('bookmarks',JSON.stringify([]));
+    }
+    bookData["bookmarks"] = JSON.parse(localStorage.getItem("bookmarks"));
 }
+
+const bookmarkButton = document.querySelector('i#bookmark');
+bookmarkButton.addEventListener('click',(e)=>{
+    // consol
+    let title = document.querySelector('.poster figcaption').textContent;
+    if(!bookData['bookmarks'].find(element=> element['title']=== title)){
+        add(title)
+    }else{
+        //Prevent Modal from Popping Up
+        e.stopPropagation();
+    }
+})
+
+function add(obj){
+    // console.log(typeof bookData)
+    
+    bookData['bookmarks'].push({title:obj});
+    localStorage.setItem("bookmarks",JSON.stringify(bookData['bookmarks']));
+}
+
+const formElem = document.querySelector("form#search");
+console.log(formElem);
+formElem.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const input = formElem.querySelector('input[type="search"]');
+    console.log(input)
+    showReview(input.value)
+    input.value = "";
+    console.log('It submitted')
+})
+// $('#button').on('click')
